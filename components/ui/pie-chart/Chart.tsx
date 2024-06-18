c5828f8d37175d43ef1f3a5929f2de14a799ab91
Alan Au <alanau@Alans-MacBook-Air.local>
2024-06-18 23:34:51 +0800

    Made pie chart dynamic

--- components/ui/pie-chart/Chart.tsx
+++ components/ui/pie-chart/Chart.tsx
@@ -1,38 +1,47 @@
 import React from "react";
 import { PieChart } from "react-minimal-pie-chart";
+import { useFoodContext } from "../../../context/FoodContext";
 
 const Chart: React.FC = () => {
+  const { food } = useFoodContext();
+
   return (
     <>
-      <section>
-        <h1>三文魚壽司</h1>
-        <PieChart
-          data={[
-            { title: "蛋白質", value: 38.9, color: "var(--primaryColor)" },
-            { title: "碳水化合物", value: 49.4, color: "var(--secondaryColor)" },
-            { title: "脂肪", value: 11.7, color: "#0000FF" },
-          ]}
-          lineWidth={35}
-          viewBoxSize={[400, 150]}
-          center={[200, 70]}
-          labelStyle={(dataIndex) => ({
-            fontSize: "0.4rem",
-            fill:
-              dataIndex === 0 ? "var(--primaryColor)" : dataIndex === 1 ? "var(--secondaryColor)" : "#0000FF",
-            whiteSpace: "pre-line",
-            fontWeight: "500",
-          })}
-          labelPosition={120}
-          label={({ dataEntry }) => `${dataEntry.title} : 
+      {food && (
+        <section>
+          <h1>{food[5].toString()}</h1>
+          <PieChart
+            data={[
+              { title: "碳水化合物", value: food[14] as number, color: "var(--secondaryColor)" },
+              { title: "脂肪", value: food[11] as number, color: "#0000FF" },
+              { title: "蛋白質", value: food[10] as number, color: "var(--primaryColor)" },
+            ]}
+            startAngle={270}
+            lineWidth={35}
+            viewBoxSize={[400, 150]}
+            center={[200, 70]}
+            labelStyle={(dataIndex) => ({
+              fontSize: "0.4rem",
+              fill:
+                dataIndex === 2
+                  ? "var(--primaryColor)"
+                  : dataIndex === 1
+                    ? "#0000FF"
+                    : "var(--secondaryColor)",
+              whiteSpace: "pre-line",
+              fontWeight: "500",
+            })}
+            labelPosition={120}
+            label={({ dataEntry }) => `${dataEntry.title} : 
           ${dataEntry.value}%`}
-        />
-        <p className="calorie">
-          卡路里 : <br /> 82.0kcal
-          <br />
-          <sup>/100克</sup>
-        </p>
-      </section>
-
+          />
+          <p className="calorie">
+            卡路里 : <br /> {food[9].toString()}kcal
+            <br />
+            <span>/100克</span>
+          </p>
+        </section>
+      )}
       <style jsx>{`
         section {
           margin: 0 auto;
@@ -54,10 +63,10 @@ const Chart: React.FC = () => {
           text-align: center;
           font-weight: bold;
           font-size: 1.5rem;
-          line-height: 2rem;
+          line-height: 1.7rem;
         }
 
-        .calorie sup {
+        .calorie span {
           font-size: 1.2rem;
         }
       `}</style>
