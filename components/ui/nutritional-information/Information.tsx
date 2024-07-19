233d604f4b3148fe9a2d29cdeed89abafdd74e53
Alan Au <alanau@Alans-MacBook-Air.local>
2024-07-19 11:04:49 +0800

    Changed serving unit

--- components/ui/nutritional-information/Information.tsx
+++ components/ui/nutritional-information/Information.tsx
@@ -18,7 +18,7 @@ const Information: React.FC = () => {
                   <th></th>
                   <th>每100克 / Per100g</th>
                   <th>
-                    每食用份量 / Per Serving <br /> (一碟 = 500g)
+                    每食用份量 / Per Serving <br /> ({food[18].toString()} = 500g)
                   </th>
                 </tr>
               </thead>
