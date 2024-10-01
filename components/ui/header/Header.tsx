81fe213ad8f07e05da3abd48f2f15125020c174d
Alan Au <alanau@Alans-MacBook-Air.local>
2024-10-01 19:28:25 +0800

    Commented header out

--- components/ui/header/Header.tsx
+++ components/ui/header/Header.tsx
@@ -3,28 +3,28 @@ import React from "react";
 const Header: React.FC = () => {
   return (
     <>
-      <header>
-        {/*<img src="/logo.webp" alt="HKU Med" className="logo" />*/}
-        <h1>營養計算機</h1>
-      </header>
+      {/*<header>*/}
+      {/*  /!*<img src="/logo.webp" alt="HKU Med" className="logo" />*!/*/}
+      {/*  <h1>營養計算機</h1>*/}
+      {/*</header>*/}
 
-      <style jsx>{`
-        header {
-          padding: 30px;
-        }
+      {/*<style jsx>{`*/}
+      {/*  header {*/}
+      {/*    padding: 30px;*/}
+      {/*  }*/}
 
-        .logo {
-          height: 50px;
-        }
+      {/*  .logo {*/}
+      {/*    height: 50px;*/}
+      {/*  }*/}
 
-        h1 {
-          text-align: center;
-          color: var(--primaryColor);
-          margin-top: 25px;
-          font-size: 1.5rem;
-          font-weight: 500;
-        }
-      `}</style>
+      {/*  h1 {*/}
+      {/*    text-align: center;*/}
+      {/*    color: var(--primaryColor);*/}
+      {/*    margin-top: 25px;*/}
+      {/*    font-size: 1.5rem;*/}
+      {/*    font-weight: 500;*/}
+      {/*  }*/}
+      {/*`}</style>*/}
     </>
   );
 };
