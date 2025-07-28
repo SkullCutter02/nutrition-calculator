465eddeaf01448e0cdb1a0045dbd390c7e8c711f
Alan Au <alanau@Alans-MacBook-Air.local>
2025-07-28 16:24:11 +0800

    Removed legend

--- components/ui/tips-section/TipsSection.tsx
+++ components/ui/tips-section/TipsSection.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import Legend from "./Legend";
 import TipBox from "./TipBox";
 import Footer from "./Footer";
 import ReferenceTables from "./ReferenceTables";
@@ -8,40 +7,11 @@ import ReferenceTables from "./ReferenceTables";
 const TipsSection: React.FC = () => {
   return (
     <>
-      <section>
-        <Legend />
-      </section>
       <ReferenceTables />
       <section>
         <TipBox />
         <Footer />
       </section>
-
-      <style jsx>{`
-        section {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          width: 60%;
-          margin: 7px auto;
-          font-weight: bold;
-          font-size: 1rem;
-        }
-
-        @media only screen and (max-width: 800px) {
-          section {
-            width: 80%;
-            font-size: 0.8rem;
-          }
-        }
-
-        @media only screen and (max-width: 650px) {
-          section {
-            width: 90%;
-            font-size: 0.7rem;
-          }
-        }
-      `}</style>
     </>
   );
 };
